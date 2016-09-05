dd8be9550df384cf734cca2289156269f2ae858d
Anh Son Le <anhsonle@Anhs-MacBook-Pro.local>
2016-09-05 17:22:22 +0700

    init project BT1

---
+++ LeAnhSon/BT1/BankCalculator/BankCalculator/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  BankCalculator
+//
+//  Created by Anh Son Le on 9/5/16.
+//  Copyright © 2016 Anh Son Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	// MARK: - Declare
+	
+	
+	// MARK: - LifeCycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	// MARK: - Private func
+	
+	
+
+}
+
