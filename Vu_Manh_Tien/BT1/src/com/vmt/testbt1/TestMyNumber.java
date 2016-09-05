fca3695126346e09bd396aec24c179382e989304
tienvm58 <tienvm_58@vnu.edu.vn>
2016-09-06 00:06:20 +0700

    fix

--- Vu_Manh_Tien/BT1/src/com/vmt/testbt1/TestMyNumber.java
+++ Vu_Manh_Tien/BT1/src/com/vmt/testbt1/TestMyNumber.java
@@ -1,18 +1,18 @@
 package com.vmt.testbt1;
 
-import com.vmt.bt1.MyNumber;
+import org.junit.Assert;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.vmt.bt1.MyNumber;
 
 public class TestMyNumber {
 
-	@Test
+	@org.junit.Test
 	public void testMyNumber(){
 		boolean test = true;
 		MyNumber myNumber = new MyNumber();
 		test = test && (myNumber.tong() > 100);
+		Assert.assertTrue(test);
+		test = test && (myNumber.tich() > 100);
 		Assert.assertTrue(test);
 	}
 }
