72894a2567763b1ac2e3c9b24a1ba5da20a94013
QuocCuongUET <cuonglh_58@vnu.edu.vn>
2016-09-05 22:48:32 +0700

    bai tap tuan 1

--- LeHuyCuong/BT1/test/BT1Test.java
+++ LeHuyCuong/BT1/test/BT1Test.java
@@ -16,15 +16,15 @@ public class BT1Test {
     @Test
     public void test2() throws Exception {
         BT1 bt = new BT1();
-        int arr[] = {4,6,8,2,-20,15};
+        int arr[] = {91,6,34,19,-20,15};
         assertEquals(1, bt.tongCacUoc(arr));
     }
 
     @Test
     public void test3() throws Exception {
         BT1 bt = new BT1();
-        int arr[] = {2,9,8,2,-21,45};
-        assertEquals(1, bt.tongCacUoc(arr));
+        int arr[] = {2,13,8,2,-26,47};
+        assertEquals(35, bt.tongCacUoc(arr));
     }
 
 }
