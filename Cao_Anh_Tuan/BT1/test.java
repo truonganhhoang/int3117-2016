d204fed63c92eecbf5eab66b8ad30eec8915574f
TuanIdol <tuanca_58@vnu.edu.vn>
2016-09-06 15:12:02 +0700

    bt1

--- Cao_Anh_Tuan/BT1/test.java
+++ Cao_Anh_Tuan/BT1/test.java
@@ -5,17 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class test {
-
-	@Test
-	public void testBP() {
-		bt1 bt = new bt1();
-		int i = bt.binhphuong(2);
-		assertEquals(4, i);
-	}
 	@Test
 	public void testDEM() {
 		bt1 bt = new bt1();
-		int u = bt.dem("kiem thu");
-		assertEquals(1, u);
+		int u = bt.dem("kiem thu va dam bao chat luong");
+		assertEquals(2, u);
 	}
 }
