69b81f7415f6361eb0ebe48cc2696c2530a4d7a6
hongnhung95 <hongnhung.tester@gmail.com>
2016-09-06 21:14:38 +0700

    Bài tập 1

---
+++ TruongThiHongNhung/BT1/SoHoanHaoTest.java
@@ -0,0 +1,37 @@
+package com.nhungtth.sohoanhao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SoHoanHaoTest {
+    @Test
+    public void test6() {
+        SoHoanHao shh = new SoHoanHao();
+        boolean result = shh.kiemTraSoHoanHao(6);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void test15() {
+        SoHoanHao shh = new SoHoanHao();
+        boolean result = shh.kiemTraSoHoanHao(15);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testNegative6() {
+        SoHoanHao shh = new SoHoanHao();
+        boolean result = shh.kiemTraSoHoanHao(-6);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testZero() {
+        SoHoanHao shh = new SoHoanHao();
+        boolean result = shh.kiemTraSoHoanHao(0);
+        assertEquals(false, result);
+    }
+
+
+}
