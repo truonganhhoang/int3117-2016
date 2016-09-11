05e33bdd04b92afba8874bce03895659e0254988
Dao Thi Thanh Huyen <huyendtt195@gmail.com>
2016-09-11 20:10:04 +0700

    Update readme.txt và thay đổi source BT1 - DaoThiThanhHuyen
    Thêm readme.txt cho BT1 và sửa code

--- DaoThiThanhHuyen/BT1/src/main/TinhDiem.java
+++ DaoThiThanhHuyen/BT1/src/main/TinhDiem.java
@@ -1,18 +1,18 @@
 package main;
-
 import java.util.Scanner;
 
 public class TinhDiem {
 	
 	public static String PhanLoai(int grade) {
-		if (grade >= 80) return "A";
+		if (grade >= 80 && grade <= 100) return "A";
 		else if (grade >= 60 && grade <= 79) return "B";
 		else if (grade >= 50 && grade <= 59) return "C";
 		else if (grade >= 40 && grade <= 49) return "D";
-		else return "F";
+		else if (grade >= 0 && grade <= 39) return "F";
+		else return "Invalid";
 	}
-	
-	public static void main(String[] args) {
+
+	public static int NhapDiem(){
 		Scanner input = new Scanner(System.in);
 		System.out.println("Nhap diem tu 0 den 100: ");
 		int grade = 0;
@@ -21,7 +21,12 @@ public class TinhDiem {
 			if(grade >= 0 && grade <= 100) break;
 			System.out.println("Diem khong hop le, nhap lai: ");
 		} while(true);
-		System.out.println(PhanLoai(grade));
+		return grade;
+	}
+	
+	public static void main(String[] args) {
+		int diem = NhapDiem();
+		System.out.println(PhanLoai(diem));
 	}
 
 }
