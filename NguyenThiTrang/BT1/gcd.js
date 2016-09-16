405280ed3327ff1bd5e8b5137b014e38622dc672
Trang2uet <trang2uet@gmail.com>
2016-09-16 22:37:32 +0700

    BT tuan 3

--- NguyenThiTrang/BT1/gcd.js
+++ NguyenThiTrang/BT1/gcd.js
@@ -1,5 +1,8 @@
 function gcd(a, b) {
-	if (a % 1 !=0 || b %1 != 0) return false;
+	if (a < 0) a = -a;
+    if (b < 0) b = -b;
+	if (Number.isSafeInteger(a) == false || Number.isSafeInteger(b) == false) return false;
+	
     if (a == 0 || b == 0) return a + b;
     while (b != 0) {
     	temp = b;
