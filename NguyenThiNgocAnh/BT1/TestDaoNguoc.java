7b55dae9dab5709ebaebc53d5fa6e22cf3ba9fa6
anhntn58 <ngocanhnguyen.uet@gmail.com>
2016-09-18 23:19:46 +0700

    Revert "Bai Tap Tuan 3"
    This reverts commit a3727356bb8c711eb6551322258d7368aa406d03.

--- NguyenThiNgocAnh/BT1/TestDaoNguoc.java
+++ NguyenThiNgocAnh/BT1/TestDaoNguoc.java
@@ -10,40 +10,21 @@ public class TestDaoNguoc {
 	public void test() {
 		
 		SoDaoNguoc num = new SoDaoNguoc();
-		//test ham doa nguoc so
-		//so doi xung
+		//SoDaoNguoc num2 = new SoDaoNguoc();
+		//test hàm đảo ngược số
+		//số đối xứng
 		int k = num.DaoNguocSo(121);
 		assertEquals(121, k);
-		//so khong doi xung
-		int s = num.DaoNguocSo(123);
-		assertEquals(321, s);
-		//test ham so sanh
-		//so doi xung
+		//số không đối xứng
+		int d = num.DaoNguocSo(123);
+		assertEquals(321, d);
+		//test hàm so sánh
+		//số đối xứng thì có số đảo ngược bằng chính nó
 		int e = num.SoSanh(22022, num.DaoNguocSo(22022));
 		assertEquals(1, e);
-		//so khong doi xung
+		//số không đối xứng không bằng số đảo ngược
 		int t = num.SoSanh(132, num.DaoNguocSo(132));
 		assertEquals(0, t);
-		//gt nho nhat
-		int a = num.SoSanh(-2147483648, num.DaoNguocSo(-2147483648));
-		assertEquals(0, a);
-		//gt ngay tren nho nhat
-		int b = num.SoSanh(-2147483647, num.DaoNguocSo(-2147483647));
-		assertEquals(0, b);
-		//mot so gt binh thuong
-		//so 0
-		int i = num.SoSanh(0, num.DaoNguocSo(0));
-		assertEquals(1, i);
-		//so -1
-		int j = num.SoSanh(-1, num.DaoNguocSo(-1));
-		assertEquals(1, j);
-		
-		//gt ngay duoi gt lon nhat
-		int c = num.SoSanh(2147483646, num.DaoNguocSo(2147483646));
-		assertEquals(0, c);
-		//gt lon nhat
-		int d = num.SoSanh(2147483647, num.DaoNguocSo(2147483647));
-		assertEquals(0, d);
 	}
 
 }
