51014d1d716a66130431773d532e6bca893c1c52
cuongdv95 <dinhvietcuong1995@gmail.com>
2016-09-19 21:53:43 +0700

    Update NumberTest.java

--- project_demo_test_dinhvietcuong/src/test/NumberTest.java
+++ project_demo_test_dinhvietcuong/src/test/NumberTest.java
@@ -15,42 +15,108 @@ public class NumberTest {
 
 	@Test
 	/**
-	 * Test trường hợp số cần tìm nằm đầu tiên
+	 * Test trường hợp mảng không có phần tử nào
 	 */
 	public void test1() {
-		int a[] = { 1, 2, 3, 4, 5 };
+		int a[] = {};
 		Number number = new Number();
-		assertEquals(1, number.search(1, a));
+		assertEquals(0, number.search(1, a));
 	}
 
 	@Test
 	/**
-	 * Test trường hợp số cần tìm nằm chính giữa trong dãy
+	 * Test trường hợp mảng có 1 phần tử, khóa tìm kiếm nhỏ hơn phần tử trong
+	 * mảng
 	 */
 	public void test2() {
-		int a[] = { 1, 2, 3, 4, 5 };
+		int a[] = { 10 };
 		Number number = new Number();
-		assertEquals(3, number.search(3, a));
+		assertEquals(0, number.search(3, a));
 	}
 
 	@Test
 	/**
-	 * Test trường hợp số cần tìm nằm cuối dãy
+	 * Test trường hợp mảng có 1 phần tử, khóa tìm kiếm lớn hơn phần tử trong
+	 * mảng
 	 */
 	public void test3() {
-		int a[] = { 1, 2, 3, 4, 5 };
+		int a[] = { 10 };
 		Number number = new Number();
-		assertEquals(5, number.search(5, a));
+		assertEquals(0, number.search(20, a));
 	}
 
 	@Test
 	/**
-	 * Test trường hợp số cần tìm không có trong dãy
+	 * Test trường hợp mảng có 1 phần tử, khóa tìm kiếm là phần tử trong mảng
 	 */
 	public void test4() {
-		int a[] = { 1, 2, 3, 4, 5 };
+		int a[] = { 10 };
 		Number number = new Number();
-		assertEquals(0, number.search(6, a));
+		assertEquals(1, number.search(10, a));
 	}
 
+	@Test
+	/**
+	 * Trường hợp mảng có nhiều hơn 1 phần tử, khóa tìm kiếm có giá trị nhỏ hơn
+	 * phần tử nhỏ nhất
+	 */
+	public void test5() {
+		int a[] = { 10, 15, 20, 25 };
+		Number number = new Number();
+		assertEquals(0, number.search(5, a));
+	}
+
+	@Test
+	/**
+	 * Trường hợp mảng có nhiều hơn 1 phần tử, khóa tìm kiếm có giá trị lớn hơn
+	 * phần tử lớn nhất
+	 */
+	public void test6() {
+		int a[] = { 10, 15, 20, 25 };
+		Number number = new Number();
+		assertEquals(0, number.search(30, a));
+	}
+
+	@Test
+	/**
+	 * Trường hợp mảng có nhiều hơn 1 phần tử, khóa tìm kiếm có giá trị nằm giữa
+	 * phần tử nhỏ nhất và phần tử lớn nhất, khóa tìm kiếm không có trong mảng
+	 */
+	public void test7() {
+		int a[] = { 10, 15, 20, 25 };
+		Number number = new Number();
+		assertEquals(0, number.search(22, a));
+	}
+
+	@Test
+	/**
+	 * Trường hợp mảng có nhiều hơn 1 phần tử, khóa tìm kiếm là phàn tử cuối
+	 * cùng
+	 */
+	public void test8() {
+		int a[] = { 10, 15, 20, 25 };
+		Number number = new Number();
+		assertEquals(4, number.search(25, a));
+	}
+
+	@Test
+	/**
+	 * Trường hợp mảng có nhiều hơn 1 phần tử, khóa tìm kiếm là phần tử đầu tiên
+	 */
+	public void test9() {
+		int a[] = { 10, 15, 20, 25 };
+		Number number = new Number();
+		assertEquals(1, number.search(10, a));
+	}
+
+	@Test
+	/**
+	 * Trường hợp mảng có nhiều hơn 1 phần tử, khóa tìm kiếm có trong mảng ở vị
+	 * trí bất kì, không phải lớn nhất cũng không phải nhỏ nhất
+	 */
+	public void test10() {
+		int a[] = { 10, 15, 20, 25 };
+		Number number = new Number();
+		assertEquals(2, number.search(15, a));
+	}
 }
