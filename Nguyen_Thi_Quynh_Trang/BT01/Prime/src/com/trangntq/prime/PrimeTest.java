16b83aff38702238636c7312b52ce8d91eb8fe14
phuongnt58 <phuong.k58uet@gmail.com>
2016-09-19 22:54:12 +0700

    update testcase

--- Nguyen_Thi_Quynh_Trang/BT01/Prime/src/com/trangntq/prime/PrimeTest.java
+++ Nguyen_Thi_Quynh_Trang/BT01/Prime/src/com/trangntq/prime/PrimeTest.java
@@ -5,6 +5,35 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class PrimeTest {
+
+    @Test
+    public void test0IsNotPrime() {
+        Prime prime = new Prime();
+        boolean result = prime.isPrime(0);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void test1IsNotPrime() {
+        Prime prime = new Prime();
+        boolean result = prime.isPrime(1);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void test2IsPrime() {
+        Prime prime = new Prime();
+        boolean result = prime.isPrime(2);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void test3IsNotPrime() {
+        Prime prime = new Prime();
+        boolean result = prime.isPrime(3);
+        assertEquals(true, result);
+    }
+
     @Test
     public void test5IsPrime() {
         Prime prime = new Prime();
@@ -16,13 +45,6 @@ public class PrimeTest {
     public void test9NotPrime() {
         Prime prime = new Prime();
         boolean result = prime.isPrime(9);
-        assertEquals(false, result);
-    }
-
-    @Test
-    public void testSmaller2NotPrime() {
-        Prime prime = new Prime();
-        boolean result = prime.isPrime(1);
         assertEquals(false, result);
     }
 
