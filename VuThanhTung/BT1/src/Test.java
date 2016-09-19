0e5cff1137c07dfe93b513d2ab6c860f974888ed
tung.vu <tung.vu@nccsoft.vn>
2016-09-20 00:31:19 +0700

    BT1 Nang Cap

--- VuThanhTung/BT1/src/Test.java
+++ VuThanhTung/BT1/src/Test.java
@@ -6,20 +6,20 @@ import static org.junit.Assert.*;
 public class Test {
 
     @org.junit.Test
-    public void test() {
+    public void test1() {
         BT1 bt1 = new BT1();
         int mang[] = {1,2,3,4,5,6};
         int tong = bt1.tongSoChan(mang);
         assertEquals(12, tong);
     }
-
     @org.junit.Test
     public void test2() {
         BT1 bt1 = new BT1();
-        int mang[] = {1,-2,3,16,5,6};
+        int mang[] = {2,16,4};
         int tong = bt1.tongSoChan(mang);
-        assertEquals(20, tong);
+        assertEquals(22, tong);
     }
+
     @org.junit.Test
     public void test3() {
         BT1 bt1 = new BT1();
@@ -31,9 +31,13 @@ public class Test {
     @org.junit.Test
     public void test4() {
         BT1 bt1 = new BT1();
-        int mang[] = {1,3,5,-12,-6};
+        int mang[] = {1,-2,3,16,5,6};
         int tong = bt1.tongSoChan(mang);
-        assertEquals(-18, tong);
+        assertEquals(20, tong);
     }
+
+
+
+
 
 }
