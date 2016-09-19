cb80b5ca5474299fc379ee2328f1646e860c711b
Max <tutv95@gmail.com>
2016-09-20 00:54:59 +0700

    Updated BT2

---
+++ TranVanTu/BT2/hack.js
@@ -0,0 +1,25 @@
+module.exports = (function () {
+    return {
+        is_prime_number: is_prime_number
+    };
+
+    function is_prime_number(number) {
+        if (isNaN(number)) {
+            return false;
+        }
+
+        if (number < 2) {
+            return false;
+        }
+
+        var primitive = [2, 3, 5, 7];
+
+        for (var i=0; i<primitive.length; i++) {
+            if (number !== primitive[i] && number % primitive[i] == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+})();
