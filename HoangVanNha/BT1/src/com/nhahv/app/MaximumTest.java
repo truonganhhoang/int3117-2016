d127dc551fe46365c6345a9ea2ee8ebb5bd9852a
nhahv0902 <nhahv0902@gmail.com>
2016-09-19 21:30:08 +0700

    change file test

--- HoangVanNha/BT1/src/com/nhahv/app/MaximumTest.java
+++ HoangVanNha/BT1/src/com/nhahv/app/MaximumTest.java
@@ -20,6 +20,88 @@ public class MaximumTest {
 
     }
 
+
+    // kiểm thử biên
+    // kiểm thử biên mạnh
+    /*
+    * kiểm thử các giá trị biên: nếu a <= y << b
+    * thì sẽ test các trường hợp: a, a+1, a + b/2, b-1, b
+    * kiểm thử biên mạnh chọn các giá trị: a-1, b+1
+    *
+    * */
+    // Chương trình mới
+    // Test với giá trị min
+    @Test
+    public void TestFuncWithValueMin() {
+        List<Integer> listTest = new ArrayList<>();
+        listTest.add(0);
+        int x = Integer.MIN_VALUE;
+        listTest.add(x);
+        assertEquals(0, maximum.maxOfList(listTest));
+    }
+
+    // Test với giá trị min + 1
+    @Test
+    public void TestFuncWithValueMinAdd1() {
+        List<Integer> listTest = new ArrayList<>();
+        listTest.add(0);
+        int x = Integer.MIN_VALUE + 1;
+        listTest.add(x);
+        assertEquals(0, maximum.maxOfList(listTest));
+    }
+
+    // Test với giá trị middle  a + b/2
+    @Test
+    public void TestFuncWithValueMiddle() {
+        List<Integer> listTest = new ArrayList<>();
+        listTest.add(0);
+        int x = Integer.MIN_VALUE + Integer.MAX_VALUE / 2;
+        listTest.add(x);
+        assertEquals(x, maximum.maxOfList(listTest));
+    }
+
+    @Test
+    public void TestFuncWithValueMax() {
+        List<Integer> listTest = new ArrayList<>();
+        listTest.add(0);
+        int x = Integer.MAX_VALUE;
+        listTest.add(x);
+        assertEquals(x, maximum.maxOfList(listTest));
+    }
+
+    @Test
+    public void TestFuncWithValueMaxSub1() {
+        List<Integer> listTest = new ArrayList<>();
+        listTest.add(0);
+        int x = Integer.MAX_VALUE - 1;
+        listTest.add(x);
+        assertEquals(x, maximum.maxOfList(listTest));
+    }
+
+    /* Kiểm thử biên mạnh*/
+
+    @Test
+    public void TestFuncWithValueMaxAdd1() {
+        List<Integer> listTest = new ArrayList<>();
+        listTest.add(0);
+        int x = Integer.MAX_VALUE + 1;
+        listTest.add(x);
+        assertEquals(x, maximum.maxOfList(listTest));
+    }
+
+     /* Kiểm thử biên mạnh*/
+
+    @Test
+    public void TestFuncWithValueMinSub1() {
+        List<Integer> listTest = new ArrayList<>();
+        listTest.add(0);
+        int x = Integer.MIN_VALUE - 1;
+        listTest.add(x);
+        assertEquals(0, maximum.maxOfList(listTest));
+    }
+
+
+    // chương trình cũ
     @Test
     public void MaximumTest1() {
         List<Integer> listTest = new ArrayList<>();
