098a59a9b4b78a83efc81eba5053066117bd2500
Kien Vu <vtk5995@gmail.com>
2016-09-25 22:17:15 +0700

    - reformat code

--- VuTrungKien/BT1/Solver.py
+++ VuTrungKien/BT1/Solver.py
@@ -14,16 +14,21 @@ class Solver:
             sentence: str
                 Input sentence, case-sensitive.
         """
-        words = sentence.split()
         word_dictionary = {}
         sorted_word_list = []
+
+        words = sentence.split()
         for word in words:
             if word in word_dictionary:
                 word_dictionary[word] += 1
             else:
                 word_dictionary[word] = 1
+
         for word, count in word_dictionary.iteritems():
             sorted_word_list.append((word, count))
+
         sorted_word_list.sort(key=lambda tup: (-tup[1], tup[0]))
 
-        return sorted_word_list[0][0] if len(sorted_word_list) else None
+        most_popular_word = sorted_word_list[0][0] if len(sorted_word_list) else None
+
+        return most_popular_word
