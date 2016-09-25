a53c051371e8166f81fa3a696bc9bd5213ef3d32
Bùi Đăng Hoàng <maskxcv@gmail.com>
2016-09-25 23:28:36 +0700

    Update TestDocSo.java

--- BuiDangHoang/BT1/test/TestDocSo.java
+++ BuiDangHoang/BT1/test/TestDocSo.java
@@ -10,33 +10,89 @@ public class TestDocSo {
 	DocSo ds = new DocSo();
 
 	@Test
-	public void testDocDonVi() {
-		assertEquals("mot", ds.docDonVi(1));
-		assertEquals("hai", ds.docDonVi(2));
-		assertEquals("ba", ds.docDonVi(3));
-		assertEquals("bon", ds.docDonVi(4));
-		assertEquals("nam", ds.docDonVi(5));
-		assertEquals("sau", ds.docDonVi(6));
-		assertEquals("bay", ds.docDonVi(7));
-		assertEquals("tam", ds.docDonVi(8));
-		assertEquals("chin", ds.docDonVi(9));
+	public void doc0() {
+		String n = ds.docSo(0);
+		assertEquals("khong", n);
 	}
-
 	@Test
-	public void testDocHangChuc() {
-		String i = ds.docHangChuc(5);
-		assertEquals("nam muoi", i);
+	public void doc1() {
+		String n = ds.docSo(1);
+		assertEquals("mot", n);
 	}
-
 	@Test
-	public void testDocSo() {
-		String i = ds.docSo(22);
-		assertEquals("hai muoi hai", i);
+	public void doc10() {
+		String n = ds.docSo(10);
+		assertEquals("muoi", n);
 	}
-
 	@Test
-	public void testTinhTong() {
-		int i = ds.tinhTong("chan", 6);
-		assertEquals(12, i);
+	public void doc11() {
+		String n = ds.docSo(11);
+		assertEquals("muoi mot", n);
 	}
+	@Test
+	public void doc15() {
+		String n = ds.docSo(15);
+		assertEquals("muoi lam", n);
+	}
+	@Test
+	public void doc20() {
+		String n = ds.docSo(20);
+		assertEquals("hai muoi", n);
+	}
+	@Test
+	public void doc21() {
+		String n = ds.docSo(21);
+		assertEquals("hai muoi mot", n);
+	}
+	@Test
+	public void doc25() {
+		String n = ds.docSo(25);
+		assertEquals("hai muoi lam", n);
+	}
+	@Test
+	public void doc99() {
+		String n = ds.docSo(99);
+		assertEquals("chin muoi chin", n);
+	}
+	@Test
+	public void doc100() {
+		String n = ds.docSo(100);
+		assertEquals("mot tram", n);
+	}
+	@Test
+	public void doc101() {
+		String n = ds.docSo(101);
+		assertEquals("mot tram linh mot", n);
+	}
+	@Test
+	public void doc110() {
+		String n = ds.docSo(110);
+		assertEquals("mot tram muoi", n);
+	}
+	@Test
+	public void doc111() {
+		String n = ds.docSo(111);
+		assertEquals("mot tram muoi mot", n);
+	}
+	@Test
+	public void doc125() {
+		String n = ds.docSo(125);
+		assertEquals("mot tram hai muoi lam", n);
+	}
+	@Test
+	public void doc901() {
+		String n = ds.docSo(901);
+		assertEquals("chin tram linh mot", n);
+	}
+	@Test
+	public void doc999() {
+		String n = ds.docSo(999);
+		assertEquals("chin tram chin muoi chin", n);
+	}
+	@Test
+	public void doc1000() {
+		String n = ds.docSo(1000);
+		assertEquals("So nam ngoai khoang doc", n);
+	}
+	
 }
