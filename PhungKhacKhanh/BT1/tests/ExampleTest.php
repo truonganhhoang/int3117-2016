b1cf5a839ccfd2ca0ba0d81d3654c69c44091d83
CloeChoy <phungkhackhanh.pkk@gmail.com>
2016-09-26 21:43:26 +0700

    BT4

--- PhungKhacKhanh/BT1/tests/ExampleTest.php
+++ PhungKhacKhanh/BT1/tests/ExampleTest.php
@@ -2,9 +2,8 @@
 require '../src/Example.php';
 class ExampleTest extends PHPUnit_Framework_TestCase
 {
-	 /**
-	 * @covers            \money\src\Example::__construct
-     * @uses              \money\src\Example
+	/**
+     * @covers Example::check
      */
     public function testOne()
     {
@@ -12,19 +11,25 @@ class ExampleTest extends PHPUnit_Framework_TestCase
 		$val = $valTest->check(0);
         $this->assertEquals(1, $val);
     }
-	
+	/**
+     * @covers Example::check
+     */
 	public function testTwo(){
 		$valTest = new Example();
 		$val = $valTest->check(10);
         $this->assertEquals(0, $val);
 	}
-	
+	/**
+     * @covers Example::check
+     */
 	public function testThree(){
 		$valTest = new Example();
 		$val = $valTest->check(-1);
         $this->assertEquals(0, $val);
 	}
-	
+	/**
+     * @covers Example::check
+     */
 	public function testFour(){
 		$valTest = new Example();
 		$val = $valTest->check(10000000000000000000000000000000000000000000000000000000000000000000000000000);
