e24ceb0cadd0661da22dc1f5f921377b0bd6078f
thunt58 <thunt58.uet@gmail.com>
2016-09-26 22:46:55 +0700

    bai tap tuan 4

--- NguyenThiThu/BT1/README.md
+++ NguyenThiThu/BT1/README.md
@@ -1,4 +1,5 @@
-#Thực hiện
+# Báo cáo bài tập tuần 1
+##Thực hiện
 
 ##Chuẩn bị
 
@@ -24,7 +25,7 @@ Right click/Run as Junit
                   <li>+ [500,1000]</li>
                   <li>+ >1000</li>
                   </ul>
-## Test case:</br>
+##Test case:</br>
 package examples.asg01;
 import org.junit.Test;
 
@@ -79,8 +80,17 @@ public class TestCustomer {
 	   }
 }
 
-#Kết quả:
-http://imgur.com/a/SlOAQ
-#Sơ đồ mô tả bài toán:
+##Kết quả chạy test case:
+http://imgur.com/a/EMLdE
+# Báo cáo bài tập tuần 4  
+##Sơ đồ mô tả bài toán:  
 http://imgur.com/a/CFCvL
+##Đo độ bao phủ của testcase:  
+##Thực hiện  
+Sử dụng EclEmma plugin for eclipse - code coverage tool
+##Chạy tool  
+Right click asg01/Coverage As
+##Kết quả  
+http://imgur.com/a/oqVUM
+
 
--- NguyenThiThu/BT1/src/test/java/examples/asg01/TestCustomer.java
+++ NguyenThiThu/BT1/src/test/java/examples/asg01/TestCustomer.java
@@ -6,12 +6,12 @@ import static org.junit.Assert.*;
 
 
 public class TestCustomer {
-		CustomerBonus customerAccount = new CustomerBonus();
 		CustomerDetails customer = new CustomerDetails();
-
-	   
+		CustomerBonus customerAccount = new CustomerBonus();
+		
+  
 	   @Test
-	   public void testBonus1() {
+	   public void testPaymentMoneyMorethan500() {
 	      customer.setName("thunt");
 	      customer.setPaymentMoney(820);
 	      customer.setBonus(0);
@@ -21,9 +21,8 @@ public class TestCustomer {
 	      assertEquals(25, bonus, 0.0);
 	   }
 
-	
 	   @Test
-	   public void testBonus2() {
+	   public void testPaymentMoneyLessthan500() {
 	      customer.setName("thunt");
 	      customer.setPaymentMoney(400);
 	      customer.setBonus(15);
@@ -32,8 +31,9 @@ public class TestCustomer {
 	      double bonus = customerAccount.calculateBonus(customer);
 	      assertEquals(15, bonus, 0.0);
 	   }
+	   
 	   @Test
-	   public void testBonus3() {
+	   public void testPaymentMoneyMorethan1000() {
 	      customer.setName("thunt");
 	      customer.setPaymentMoney(1200);
 	      customer.setBonus(15);
@@ -42,14 +42,14 @@ public class TestCustomer {
 	      double bonus = customerAccount.calculateBonus(customer);
 	      assertEquals(50, bonus, 0.0);
 	   }
+	   
 	   @Test
-	   public void testBonus4() {
+	   public void testFailPaymentMoney() {
 	      customer.setName("thunt");
 	      customer.setPaymentMoney(-0.5);
 	      customer.setBonus(0);
 	      customer.setTime(0);
-//	      System.out.println("nhap lai so tien tra");
 	      double bonus = customerAccount.calculateBonus(customer);
-	      assertEquals(-0.03, bonus, 0.0);
+	      equals("so tien tra khong dung");
 	   }
 }
