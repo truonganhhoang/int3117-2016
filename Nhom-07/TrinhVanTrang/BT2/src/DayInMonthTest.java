7c47d69989156729392daf303f1a63298a19df73
trangtv58 <trangtv95@gmail.com>
2016-10-04 00:29:42 +0700

    update bt2

--- Nhom-07/TrinhVanTrang/BT2/src/DayInMonthTest.java
+++ Nhom-07/TrinhVanTrang/BT2/src/DayInMonthTest.java
@@ -13,6 +13,48 @@ public class Day_In_Month_Test {
 	}
 	
 	@Test
+	public void Month_3(){
+		DayInMonth test = new DayInMonth();
+		int x = DayInMonth.Number_Day_In_Month(3,2004);
+		assertEquals(31,x);
+	}
+	
+	@Test
+	public void Month_5(){
+		DayInMonth test = new DayInMonth();
+		int x = DayInMonth.Number_Day_In_Month(5,2004);
+		assertEquals(31,x);
+	}
+	
+	@Test
+	public void Month_7(){
+		DayInMonth test = new DayInMonth();
+		int x = DayInMonth.Number_Day_In_Month(7,2004);
+		assertEquals(31,x);
+	}
+	
+	@Test
+	public void Month_8(){
+		DayInMonth test = new DayInMonth();
+		int x = DayInMonth.Number_Day_In_Month(8,2004);
+		assertEquals(31,x);
+	}
+	
+	@Test
+	public void Month_10(){
+		DayInMonth test = new DayInMonth();
+		int x = DayInMonth.Number_Day_In_Month(8,2004);
+		assertEquals(31,x);
+	}
+	
+	@Test
+	public void Month_12(){
+		DayInMonth test = new DayInMonth();
+		int x = DayInMonth.Number_Day_In_Month(8,2004);
+		assertEquals(31,x);
+	}
+	
+	@Test
 	public void Month_2_leap_year(){
 		DayInMonth test = new DayInMonth();
 		int x = DayInMonth.Number_Day_In_Month(2,2004);
@@ -25,6 +67,21 @@ public class Day_In_Month_Test {
 		int x = DayInMonth.Number_Day_In_Month(2,2015);
 		assertEquals(28,x);
 	}
+	
+	@Test
+	public void Month_12_common_year(){
+		DayInMonth test = new DayInMonth();
+		int x = DayInMonth.Number_Day_In_Month(12,2015);
+		assertEquals(31,x);
+	}
+	
+	@Test
+	public void Month_12_leap_year(){
+		DayInMonth test = new DayInMonth();
+		int x = DayInMonth.Number_Day_In_Month(12,2004);
+		assertEquals(31,x);
+	}
+	
 	
 	@Test
 	public void Month_4(){
