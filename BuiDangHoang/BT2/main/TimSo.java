5194b525c726026ce13717d3e69557e49915e535
Bùi Đăng Hoàng <maskxcv@gmail.com>
2016-10-03 21:07:45 +0700

    Update BT2

--- BuiDangHoang/BT2/main/TimSo.java
+++ BuiDangHoang/BT2/main/TimSo.java
@@ -1,8 +1,5 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TimSo {
 
 	public boolean kiemTraSo(int number) {
@@ -13,22 +10,6 @@ public class TimSo {
 			if ((number % 3 == 0 & number % 5 == 0) | number % 7 == 0) {
 				return true;
 			}
-		}
-		return result;
-	}
-
-	public List<Integer> tim(int number) {
-		List<Integer> result = new ArrayList<Integer>();
-		int n = 0;
-		while (n < number) {
-			if (0 < n | n > 1000) {
-				return result;
-			} else {
-				if (kiemTraSo(number)) {
-					result.add(n);
-				}
-			}
-			n++;
 		}
 		return result;
 	}
