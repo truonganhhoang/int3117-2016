560f5ea0ce6c058f7e265a96d24fbba0242723a2
Nguyen Tuynh <nguyenvantuynh95@gmail.com>
2016-10-03 20:43:44 +0700

    Update BT2

---
+++ MaVanTu/BT1/JUnitDemo/src/MinClass.java
@@ -0,0 +1,13 @@
+public class MinClass {
+	public int findMin(int[] arr) {
+		int min = arr[0];
+		int n = arr.length;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		return min;
+	}
+
+}
