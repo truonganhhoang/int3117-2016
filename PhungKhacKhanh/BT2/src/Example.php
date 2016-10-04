c25935b01f0912fa780d66211e6e5e15abf39923
CloeChoy <phungkhackhanh.pkk@gmail.com>
2016-10-04 16:47:50 +0700

    Update BT2

--- PhungKhacKhanh/BT2/src/Example.php
+++ PhungKhacKhanh/BT2/src/Example.php
@@ -1,18 +1,23 @@
 <?php
 	class Example {
+		const num = 100;
 		public function check($a){
 			try{
-				if($a < 10 && $a >= 0){
-					for($i = 0; $i <= $a; $i++){
-						echo $i .'<br>';
-					}
-					return 1;
-				}else{
-					throw new Exception("Value must be less than 10 and more than 0");
-					return 0;
-				} 
+				if($a <= self::num){
+					if(($a < 10 && $a >= 0) || ($a >= 10 && $a < 100)){
+						$i = 0;
+						while($i <= $a){
+							echo $i.'<br>';
+							$i++;
+						}
+						return 1;
+					}else{
+						throw new Exception("Value must be in [0;10) or [10;100)");
+					} 
+				}
 			}catch(Exception $e) {
 			  echo 'Message: ' .$e->getMessage();
+			  return 0;
 			}
 		}
 	}
