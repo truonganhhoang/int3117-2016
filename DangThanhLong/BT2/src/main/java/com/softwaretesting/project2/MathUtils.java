24f686abddb9d207edc11e4f21700522cae04b3d
longdt03 <longdt58@gmail.com>
2016-10-25 01:49:05 +0700

    update project 2

--- DangThanhLong/BT2/src/main/java/com/softwaretesting/project2/MathUtils.java
+++ DangThanhLong/BT2/src/main/java/com/softwaretesting/project2/MathUtils.java
@@ -10,11 +10,12 @@ package com.softwaretesting.project2;
  * @author DangThanh
  */
 public class MathUtils {
+
     public int findGreatestCommonDivisor(int a, int b) {
         if (a == b || a != 0 && b == 0) {
             return Math.abs(a);
         }
-        if (b != 0 && a == 0) {
+        if (a == 0 && b != 0) {
             return Math.abs(b);
         }
         int temp = 0;
