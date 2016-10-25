2b77d3a319de77d878e0f178108a89b7c2a3e5e1
tranglt58 <luongtrang151095@gmai.com>
2016-10-25 08:39:46 +0700

    BT3/CheckLeapYear.java

---
+++ LuongThiTrang/BT3/CheckLeapYear.java
@@ -0,0 +1,44 @@
+package leapYear;
+
+
+import java.util.Scanner;
+
+
+
+
+public class CheckLeapYear {
+	public static String NOT_RIGHT_LEAP_YEAR = "Not right leap year";
+	public static String RIGHT_LEAP_YEAR = "Right leap year";
+	public static String INVALID_YEAR = "Invalid year";
+	
+	
+		
+	public void  inputYear(int year) {
+		Scanner sc = new Scanner(System.in);
+		boolean kt = false;
+		System.out.println("Input Year: ");
+		while(!kt) {
+			year = sc.nextInt();
+			if(year <=0){
+				System.out.println(INVALID_YEAR);
+			} else {
+				kt = true;
+				System.out.println(checkYear(year));
+			}
+		}
+	}
+	public String checkYear(int year){
+		if ((year%4==0 && year%100!=0) || year%400==0) {
+        	return RIGHT_LEAP_YEAR;
+        }
+         else {
+            return NOT_RIGHT_LEAP_YEAR;
+        }
+}
+	
+	public static void main(String[] args){
+		CheckLeapYear year = new CheckLeapYear();
+		year.inputYear(1994);
+	}
+}
+
