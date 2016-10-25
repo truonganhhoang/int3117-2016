3ac65369201abcd40b14cc78f27f2905fd0be940
ROBERTCANCRIUS\rober <dungntm_58@vnu.edu.vn>
2016-10-25 10:02:38 +0700

    Up BT3

---
+++ NguyenTrongMinhDung/BT3/Tala/src/test/java/com/robert/Tala/CommonCardTest.java
@@ -0,0 +1,64 @@
+package com.robert.Tala;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import com.robert.Tala.CommonCard.Suit;
+
+public class CommonCardTest {
+	CommonCard card1, card2;
+	
+	@Before
+	public void setUp(){
+		card1 = new CommonCard(3, CommonCard.Suit.spades, 1);
+		card2 = Pack.getCardAt(5);
+	}
+	
+	@Test
+	public void testToString(){
+		assertEquals("3 Bich", card1.toString());
+	}
+	
+	/*
+	@Test
+	public void testInit() {
+		try {
+			CommonCard card = new CommonCard("2 Bich");
+			assertEquals(2, card.getCardNumber());
+			assertEquals(Suit.spades, card.getSuit());
+			
+			card = new CommonCard("A Bich");
+			assertEquals(14, card.getCardNumber());
+			assertEquals(Suit.spades, card.getSuit());
+			
+			card = new CommonCard("A Co");
+			assertEquals(14, card.getCardNumber());
+			assertEquals(Suit.hearts, card.getSuit());
+		}
+		catch(Exception e) {}
+	}
+	*/
+	
+	@Test
+	public void testCardEquivalentFalse() {
+		assertFalse(card1.equals(Pack.getCurrentPack()));
+		
+		assertFalse(card1.equals(card2));
+	}
+	
+	@Test
+	public void testCardEquivalentTrue() {
+		card2 = Pack.getCardAt(4);
+		
+		assertTrue(card1.equals(card1));
+		assertTrue(card1.equals(card2));
+	}
+	
+	@Test
+	public void testCardClone() {
+		CommonCard card3 = card1.clone();
+		
+		assertTrue(card1.equals(card3));
+	}
+}
