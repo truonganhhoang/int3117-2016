714443f4473f212ae1d841d48ec8ffe67bb377d7
Nuôi cái bềm <ndhieu026@gmail.com>
2016-11-03 01:52:39 +0700

    Update TinhTongTest.java

--- NguyenDuyHieu/BT2/src/TinhTongTest.java
+++ NguyenDuyHieu/BT2/src/TinhTongTest.java
@@ -15,7 +15,7 @@ public class TinhTongTest {
 
     @Test
     public void testMCDC1() {
-        // A true, B false and C true
+        // A true, B false and C true => OUTPUT true 
         TinhTong tong = new TinhTong( 3, 4, 6);
         int sum = tong.tinhTong();
         assertEquals(1683, sum);
@@ -23,7 +23,7 @@ public class TinhTongTest {
     
     @Test
     public void testMCDC2() {
-        // A true, B false and C false       
+        // A true, B false and C false => OUTPUT false       
         TinhTong tong = new TinhTong( 3, 4, 5);      
         int sum = tong.tinhTong();     
         assertEquals(1200, sum);      
@@ -31,7 +31,7 @@ public class TinhTongTest {
 
     @Test
     public void testMCDC3() {
-        // A true, B true and C false
+        // A true, B true and C false => OUTPUT true
         TinhTong tong = new TinhTong( 3, 6, 5);
         int sum = tong.tinhTong();
         assertEquals(1683, sum);
@@ -39,7 +39,7 @@ public class TinhTongTest {
     
     @Test
     public void testMCDC4() {
-        // A false, B true and C false
+        // A false, B true and C false => OUTPUT false
         TinhTong tong = new TinhTong( 1, 3, 5);
         int sum = tong.tinhTong();
         assertEquals(1683, sum);
