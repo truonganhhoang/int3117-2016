04a1a74932b333b21d75b9e85a4ee5d3d85eb315
thangpvt58 <nvq681995@gmail.com>
2016-11-10 12:12:25 +0700

    BT1

--- PhungVanThang/BT1/src/thangpvt58/Triangle.java
+++ PhungVanThang/BT1/src/thangpvt58/Triangle.java
@@ -3,13 +3,15 @@ package thangpvt58;
 public class Triangle {
 		public boolean not(int a,int b, int c){
 			
-			if(a>b || b>c){
-				if(a < (b+c)){
-					return true;
+			while(c!0){
+				if(a>b && b>c){
+					if(a < (b+c)){
+						return true;
+					}
+					else return false;
 				}
 				else return false;
+				
 			}
-			else return false;
-			
-		}
+			return false;
 }
