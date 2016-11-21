34b12e6c064100408854d2745e27c97fb225879d
Tumv58 <mavantusdtq@gmail.com>
2016-11-21 19:58:19 +0700

    Update BT1

--- MaVanTu/BT1/JUnitDemo/src/TestMinClass.java
+++ MaVanTu/BT1/JUnitDemo/src/TestMinClass.java
@@ -7,35 +7,35 @@ public class TestMinClass {
 
 	// Test gia tri nho nhat
 	@Test
-	public void testFindMin1() {
+	public void testMinValue() {
 		int arr[] = { 1, 3, 5, -2, 9, 4, 2, 100, 0, -2147483648 };
 		assertEquals(Integer.MIN_VALUE, minClass.findMin(arr));
 	}
 
 	// Test ngay tren gia tri nho nhat
 	@Test
-	public void testFindMin2() {
+	public void testNearMinValue() {
 		int arr[] = { 1, 3, 5, -2, 9, 4, 2, 100, 0, -2147483647 };
 		assertEquals(Integer.MIN_VALUE + 1, minClass.findMin(arr));
 	}
 
 	// Test gia tri binh thuong
 	@Test
-	public void testFindMin3() {
+	public void testNormalValue() {
 		int arr[] = { 1, 3, 5, -2, 9, 4, 2, 100, 0, -9 };
 		assertEquals(-9, minClass.findMin(arr));
 	}
 
 	// Test ngay duoi gia tri lon nhat
 	@Test
-	public void testFindMin4() {
+	public void testNearMaxValue() {
 		int arr[] = { 1, 3, 5, -2, 9, 4, 2, 100, 0, 2147483646 };
 		assertEquals(-2, minClass.findMin(arr));
 	}
 
 	// Test gia tri lon nhat
 	@Test
-	public void testFindMin5() {
+	public void testMaxValue() {
 		int arr[] = { 1, 3, 5, -2, 9, 4, 2, 100, 0, 2147483647 };
 		assertEquals(-2, minClass.findMin(arr));
 	}
