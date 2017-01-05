fd0680a3f7ce3eb030791ebe20124b67dd09d350
tuenv58 <nguyentue.uet.k58@gmail.com>
2017-01-05 14:49:54 +0700

    cap nhat bai tap

--- NguyenVanTue/BT1/Demo.java
+++ NguyenVanTue/BT1/Demo.java
@@ -2,10 +2,10 @@ package baitaptuan1;
 
 public class Demo {
 	public int DemSoLe(int[] mang) {
-		int a=0;
+		int count=0;
 		for(int i=0;i<mang.length;i++){
-			if(mang[i]%2 !=0) a=a+1;
+			if(mang[i]%2 !=0) count=count+1;
 		}
-		return a;
+		return count;
 	}
 }
